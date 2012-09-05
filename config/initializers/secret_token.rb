2e5f31b53e5783c45c733829bd5fc0436b786644
Nestor Waldyd <nwaldyd@yahoo.co.uk>
2012-09-05 09:33:05 -0500

    master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JudonRails::Application.config.secret_token = '50258f7ce76835fc3acf6cf76a5ecdec752a117e248dcaeedb4285e4860ef12add646875678ac75a89be95d18f68ee5ee3726dee75f33094ed53d2e24ef8d262'
